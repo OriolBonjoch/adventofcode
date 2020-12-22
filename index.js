84c95114bb386fed2b447a6bf1ab84fd66c9c8dd
Oriol Bonjoch <oriol.bonjoch@gmail.com>
2020-12-22 01:28:45 +0100

    puzzle mounted

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('data.txt', 'utf8').split(/[\r\n]{4}/);
-const toNumbers = (border) => {
+const toHashNumber = (border) => {
   const bin = border.replace(/[\.#]/g, m => m === '#' ? 1 : 0);
-  return parseInt(bin, 2);
-  // return [parseInt(bin, 2), parseInt(bin.split('').reverse().join(''), 2)];
+  return Math.min(parseInt(bin, 2), parseInt(bin.split('').reverse().join(''), 2));
 }
+
 const tiles = data.map(tileData => {
   const lines = tileData.split(/[\r\n]+/);
   const m = lines[0].match(/Tile (\d+):/);
   const tile = parseInt(m[1]);
-  const content = lines.slice(1).filter(l => /^[^\s\n\r]*$/.test(l));
+  const content = lines.slice(1).filter(l => /^[.#]+$/.test(l));
   const border = [
     content[0],
     content.map(l => l[l.length -1]).join(''),
     content[content.length - 1].split('').reverse().join(''),
     content.map(l => l[0]).reverse().join('')
-  ].map(l => Math.min(toNumbers(l)));
-  const raw = content.slice(1, content.length-1).map(l => l.substr(1, l.length - 2));
-  return { tile, border, raw, content };
+  ];
+  const borderHash = border.map(l => toHashNumber(l));
+  const raw = content.slice(1, content.length - 1).map(l => l.substr(1, l.length - 2));
+  return { tile, border, borderHash, raw };
 }).map((t1, _, arr) => {
-  const possibles = [0,1,2,3].map(i => arr.filter(t2 => t2.tile !== t1.tile && t2.border.includes(t1.border[i]))
-    .map(t2 => t2.tile)).map(t => t);
+  const possibles = [0,1,2,3].map(i => arr.filter(t2 => t2.tile !== t1.tile && t2.borderHash.includes(t1.borderHash[i]))
+    .map(t2 => t2.tile));
   return { ...t1, possibles };
 });
 
@@ -57,8 +58,9 @@ function calculate(tiles, puzzle, i, max) {
 }
 
 function rotate(tile) {
-  const { raw } = tile;
+  const { raw, content } = tile;
   const border = [...tile.border.slice(1), tile.border[0]];
+  const borderHash = [...tile.borderHash.slice(1), tile.borderHash[0]];
   const newRaw = [];
   for (let i = 0; i < raw.length; ++i) {
     newRaw[i] = '';
@@ -68,51 +70,89 @@ function rotate(tile) {
   }
 
   tile.border = border;
+  tile.borderHash = borderHash;
+  tile.raw = newRaw;
+}
+
+function flip(tile) {
+  const { raw, content } = tile;
+  const border = [2,1,0,3].map(i => tile.border[i].split('').reverse().join(''));
+  const borderHash = [2,1,0,3].map(i => tile.borderHash[i]);
+  const newRaw = [];
+  for (let i = 0; i < raw.length; ++i) {
+    newRaw[i] = raw[raw.length - i - 1];
+  }
+
+  tile.border = border;
+  tile.borderHash = borderHash;
   tile.raw = newRaw;
 }
 
 function validateRotation(puzzleTiles, i, max) {
-  if (i % max !== 0 && puzzleTiles[i].border[3] !== puzzleTiles[i - 1].border[1]) {
-    // console.log(`Tile #${i}: ${puzzleTiles[i].border[3]} !== ${puzzleTiles[i-1].border[1]}`);
+  if (i % max !== 0 && puzzleTiles[i].border[3] !== puzzleTiles[i - 1].border[1].split('').reverse().join('')) {
     return false;
   }
-  if (i >= max && puzzleTiles[i].border[0] !== puzzleTiles[i - max].border[2]) {
-    // console.log(`Tile #${i}: ${puzzleTiles[i].border[0]} !== ${puzzleTiles[i-max].border[2]}`);
+  if (i >= max && puzzleTiles[i].border[0] !== puzzleTiles[i - max].border[2].split('').reverse().join('')) {
     return false;
   }
 
   return true;
 }
 
-function mountPuzzle(tiles, puzzle, max) {
-  const puzzleTiles = puzzle.map(id => tiles.find(t => t.tile === id));
-  console.log(`Tile #${puzzleTiles[0].tile} --- ${puzzleTiles[0].border}`);
-  let i0 = puzzleTiles[0].border.findIndex(b => puzzleTiles[1].border.includes(b));
-  while(i0 !== 1) {
-    rotate(puzzleTiles[0]);
-    console.log(` - rotate: ${puzzleTiles[0].border}`);
-    i0 = (i0 + 3) % 4;
-  }
-
-  for (let idx = 1; idx < 2; ++idx) {
-    console.log(`Tile #${puzzleTiles[idx].tile} --- ${puzzleTiles[idx].border}`);
-    for (let j = 0; j < 4 && !validateRotation(puzzleTiles, idx, max); j++) {
-      rotate(puzzleTiles[idx]);
-      console.log(` - rotate: ${puzzleTiles[idx].border}`);
-    }
-  }
-
-  return puzzleTiles.filter((_, i) => i <= 1).map(t => t.content).reduce((acc,cur,idx,arr) => {
-    const off = max * (idx - idx % max);
+function joinTiles(puzzleTiles, max) {
+  return puzzleTiles.map(t => t.raw).reduce((acc,cur,idx) => {
+    const off = cur.length * (idx - idx % max) / max;
     for (let j = 0; j < cur.length; ++j) {
-      acc[off + j] = idx % max === 0 ? cur[j] : acc[off + j] + ' ' + cur[j];
+      acc[off + j] = idx % max === 0 ? cur[j] : acc[off + j] + cur[j];
     }
     return acc;
   },[]);
 }
 
+function mountPuzzle(tiles, puzzle, max) {
+  const puzzleTiles = puzzle.map(id => tiles.find(t => t.tile === id));
+  let i0 = puzzleTiles[0].borderHash.findIndex(b => puzzleTiles[1].borderHash.includes(b));
+  while(i0 !== 1) {
+    rotate(puzzleTiles[0]);
+    i0 = (i0 + 3) % 4;
+  }
+
+  
+  for (let idx = 1; idx < puzzleTiles.length; ++idx) {
+    let isValid = validateRotation(puzzleTiles, idx, max);
+    for (let j = 0; j < 4 && !isValid; j++) {
+      rotate(puzzleTiles[idx]);
+      isValid = validateRotation(puzzleTiles, idx, max);
+    }
+
+    if (isValid) continue;
+    flip(puzzleTiles[idx]);
+    isValid = validateRotation(puzzleTiles, idx, max);
+    for (let j = 0; j < 4 && !isValid; j++) {
+      rotate(puzzleTiles[idx]);
+      isValid = validateRotation(puzzleTiles, idx, max);
+    }
+
+    if (!isValid) {
+      throw 'tile does not match';
+    }
+  }
+
+  return joinTiles(puzzleTiles, max);
+}
+
+function findMonsters(mounted) {
+  /^[.#O]{18}#[.#O]$/
+}
+
 // 1283
 const puzzle = calculate(tiles, [borderTiles[0]], 1, Math.sqrt(tiles.length));
-const mounted = mountPuzzle(tiles, puzzle, Math.sqrt(tiles.length));
+if (!puzzle) {
+  console.log('Cannot find puzzle');
+  console.log(borderTiles);
+  console.log(tiles.length);
+  return;
+}
+const mountedPuzzle = mountPuzzle(tiles, puzzle, Math.sqrt(tiles.length));
 
-console.log(mounted);
+console.log(mountedPuzzle);
