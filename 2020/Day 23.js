309666fbc093b6ba44cd9ac4aa37afa7a1b1d6a3
Oriol Bonjoch <oriol.bonjoch@gmail.com>
2020-12-24 02:14:05 +0100

    Day 23 part 2

--- 2020/Day 23.js
+++ 2020/Day 23.js
@@ -1,30 +1,72 @@
-const data = '123487596'.split('').map(x => parseInt(x));
+const init = '123487596'.split('').map(x => parseInt(x));
+const data = init.reduce((acc, cur, idx, arr) => {
+  acc[cur] = arr[(idx + 1) % arr.length];
+  return acc;
+}, [ init[0] ]);
 
-const prettyPrint = (cups, cur) => cups.map((x, i) => i === cur ? `(${x})` : x).join(' ');
-const print = (cups) => parseInt([...cups.slice(cups.indexOf(1) +1), ...cups.slice(0, cups.indexOf(1))].join(''));
+function prettyPrint(cups, max = -1)
+{
+  let result = `(${cups[0]})`;
+  let pointer = cups[cups[0]];
+  while (pointer !== cups[0] && max-- !== 0) {
+    result += ` ${pointer}`;
+    pointer = cups[pointer];
+  }
 
-function calculate(cups, current, move) {
-  if (!move) return cups;
-  current %= cups.length;
-  // console.log(prettyPrint(cups, current));
-  const picks = cups.slice(current+1, current+4);
-  const remaining = [...cups.slice(0, current+1), ...cups.slice(current+4)].slice(3-picks.length);
-  picks.push(...cups.slice(0, 3-picks.length));
-  const d = remaining.filter(x => x < cups[current]);
-  const dest = d.length ? Math.max(...d) : Math.max(...remaining);
-  const next = [...remaining.slice(0,remaining.indexOf(dest)+1), ...picks, ...remaining.slice(remaining.indexOf(dest)+1)];
-  const diff = next.indexOf(cups[current]) - current;
-
-  // console.log(' - picks', picks);
-  // console.log(' - dest', dest);
-  // console.log(' - diff', diff);
-  // console.log('remaining', remaining);
-  return calculate(
-    [...next.slice(diff), ...next.slice(0, diff)],
-    current + 1,
-    move -1);
+  return result.trimEnd();
 }
 
+function print(cups)
+{
+  let result = '';
+  let pointer = cups[1];
+  while (pointer !== 1) {
+    result += `${pointer}`;
+    pointer = cups[pointer];
+  }
 
-const result = calculate(data, 0, 100);
-console.log('Part 1', print(result));
+  return parseInt(result);
+}
+
+function calculate(cups, move) {
+  while (move--)
+  {
+    const picks = [0,1,2].reduce(acc => ({
+      pointer: cups[acc.pointer],
+      result: [...acc.result, acc.pointer]
+    }), { pointer: cups[cups[0]], result: [] }).result;
+    let dest = cups[0] === 1 ? cups.length - 1 : cups[0] - 1;
+    while (picks.includes(dest)) {
+      dest = dest === 1 ? cups.length - 1 : dest - 1;
+    }
+
+    const orig = cups[0];
+    
+    cups[orig] = cups[picks[2]];
+    cups[picks[2]] = cups[dest];
+    cups[dest] = picks[0];
+    cups[0] = cups[orig];
+  }
+
+  return cups;
+}
+
+function fillData(initial, total) {
+  const data = [...initial];
+  data[data.lastIndexOf(data[0])] = data.length;
+  for (let i = data.length; i <= total; ++i) {
+    data.push(i === total ? data[0] : i + 1);
+  }
+
+  return data;
+}
+
+const resultP1 = calculate([...data], 100);
+console.log('Part 1', print(resultP1));
+
+const crabData = fillData([...data], 1000000);
+const resultP2 = calculate(crabData, 10000000);
+
+const first = resultP2[1];
+const second = resultP2[first];
+console.log('Part 2', first * second);
--- index.js
+++ index.js
@@ -1,68 +1,72 @@
-const fs = require('fs');
+const init = '123487596'.split('').map(x => parseInt(x));
+const data = init.reduce((acc, cur, idx, arr) => {
+  acc[cur] = arr[(idx + 1) % arr.length];
+  return acc;
+}, [ init[0] ]);
 
-const data = fs.readFileSync('data.txt', 'utf8').split(/[\r\n]{4}/);
-const players = data.map(tileData => {
-  const lines = tileData.split(/[\r\n]+/);
-  const m = lines[0].match(/Player (\d+):/);
-  const deck = lines.slice(1).map(l => l.match(/^\d+$/)).filter(m => m).map(m => parseInt(m[0]));
-  return {
-    player: parseInt(m[1]),
-    deck
-  };
-});
-
-function playCombat(p1, p2) {
-  if (!p2.length) return { winner: 1, deck: p1 };
-  if (!p1.length) return { winner: 2, deck: p2 };
-  return p1[0] > p2[0] ?
-    playCombat([...p1.slice(1), p1[0], p2[0]], p2.slice(1)) :
-    playCombat(p1.slice(1), [...p2.slice(1), p2[0], p1[0]]);
-}
-
-function calculateScore(deck) {
-  return deck.reverse().reduce((acc, cur, idx) => acc + cur * (idx + 1), 0);
-}
-
-const combatWinner = playCombat(players[0].deck, players[1].deck);
-const combatScore = calculateScore(combatWinner.deck);
-console.log('Part 1', combatScore);
-
-function hash(p1,p2) {
-  return `${p1.join(',')}|${p2.join(',')}`;
-}
-
-const cache = {
-  '17,50,14,45,5,42,18,33,7,48,21,39,4,36,1,35,24,32,3,40,9,38,23,27,2,34|43,25,31,8,44,10,41,22,49,12,47,19,46,20,30': 1,
-  '43,25,31,8,44,10,41,22,49,12,47,19,46,20,30|17,50,14,45,5,42,18,33,7,48,21,39,4,36,1,35,24,32,3,40,9,38,23,27,2,34': 1,
-  '17,3,50,45,42,26,48,39,22,5,47,7,40,38,35,10,41,27,34,18,25,21,23,13,43,33,4,1,36|44,9,24,2,15,8,30,12,19,14,49,32,20,6,46,31': 1,
-  '44,9,24,2,15,8,30,12,19,14,49,32,20,6,46,31|17,3,50,45,42,26,48,39,22,5,47,7,40,38,35,10,41,27,34,18,25,21,23,13,43,33,4,1,36': 1
-};
-function playRecursiveCombat(p1, p2, historic = [], gameId = 0) {
-  // if (historic.length % 10 === 0 && gameId === 0) console.log(`round #${historic.length} (${Object.keys(cache).length} cached)`);
-  const h = hash(p1,p2);
-  if (!p2.length) return { winner: 1, deck: p1 };
-  if (!p1.length) return { winner: 2, deck: p2 };
-  if (historic.includes(h)) return { winner: 1, deck: p1, stalling: true };
-  if (p1[0] < p1.length && p2[0] < p2.length) {
-    const subGameHash = hash(p1.slice(1, p1[0]+1), p2.slice(1, p2[0]+1));
-    let subGameWinner = cache[subGameHash];
-    if (!subGameWinner) {
-      const subgame = playRecursiveCombat(p1.slice(1, p1[0]+1),p2.slice(1, p2[0]+1), [], gameId + 1);
-      subGameWinner = subgame.winner;
-      cache[subGameHash] = subgame.winner;
-      cache[subGameHash.split('|').reverse().join('|')] = subgame.winner === 2 || subgame.stalling ? 1 : 2;
-    }
-    
-    return subGameWinner === 1 ?
-      playRecursiveCombat([...p1.slice(1), p1[0], p2[0]], p2.slice(1), [...historic, h], gameId) :
-      playRecursiveCombat(p1.slice(1), [...p2.slice(1), p2[0], p1[0]], [...historic, h], gameId);
+function prettyPrint(cups, max = -1)
+{
+  let result = `(${cups[0]})`;
+  let pointer = cups[cups[0]];
+  while (pointer !== cups[0] && max-- !== 0) {
+    result += ` ${pointer}`;
+    pointer = cups[pointer];
   }
 
-  return p1[0] > p2[0] ?
-    playRecursiveCombat([...p1.slice(1), p1[0], p2[0]], p2.slice(1), [...historic, h], gameId) :
-    playRecursiveCombat(p1.slice(1), [...p2.slice(1), p2[0], p1[0]], [...historic, h], gameId);
+  return result.trimEnd();
 }
 
-const recCombatWinner = playRecursiveCombat(players[0].deck, players[1].deck);
-const recCombatScore = calculateScore(recCombatWinner.deck);
-console.log('Part 2', recCombatScore);
+function print(cups)
+{
+  let result = '';
+  let pointer = cups[1];
+  while (pointer !== 1) {
+    result += `${pointer}`;
+    pointer = cups[pointer];
+  }
+
+  return parseInt(result);
+}
+
+function calculate(cups, move) {
+  while (move--)
+  {
+    const picks = [0,1,2].reduce(acc => ({
+      pointer: cups[acc.pointer],
+      result: [...acc.result, acc.pointer]
+    }), { pointer: cups[cups[0]], result: [] }).result;
+    let dest = cups[0] === 1 ? cups.length - 1 : cups[0] - 1;
+    while (picks.includes(dest)) {
+      dest = dest === 1 ? cups.length - 1 : dest - 1;
+    }
+
+    const orig = cups[0];
+    
+    cups[orig] = cups[picks[2]];
+    cups[picks[2]] = cups[dest];
+    cups[dest] = picks[0];
+    cups[0] = cups[orig];
+  }
+
+  return cups;
+}
+
+function fillData(initial, total) {
+  const data = [...initial];
+  data[data.lastIndexOf(data[0])] = data.length;
+  for (let i = data.length; i <= total; ++i) {
+    data.push(i === total ? data[0] : i + 1);
+  }
+
+  return data;
+}
+
+const resultP1 = calculate([...data], 100);
+console.log('Part 1', print(resultP1));
+
+const crabData = fillData([...data], 1000000);
+const resultP2 = calculate(crabData, 10000000);
+
+const first = resultP2[1];
+const second = resultP2[first];
+console.log('Part 2', first * second);
